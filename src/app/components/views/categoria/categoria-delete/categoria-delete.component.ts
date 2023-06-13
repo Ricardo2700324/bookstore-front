a24893043b072b03f52296e9008f268496cc3b6a
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2023-06-13 17:10:29 -0300

    implementando categoria delete parte 2

--- src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
+++ src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from '../categoria.service';
+import { ActivatedRoute } from '@angular/router';
+import { Categoria } from '../categoria.model';
 
 @Component({
   selector: 'app-categoria-delete',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaDeleteComponent implements OnInit {
 
-  constructor() { }
+  categoria: Categoria = {
+    id: '',
+    nome: '',
+    descricao: ''
+  }
+
+  constructor(private service: CategoriaService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.categoria.id = this.route.snapshot.paramMap.get('id');
+    this.findById()
+  }
+
+  findById(): void {
+    this.service.findById(this.categoria.id).subscribe((resposta) => {
+      this.categoria.nome = resposta.nome
+      this.categoria.descricao = resposta.descricao
+    });
   }
 
 }
