39d862796a0c64aff56b849783d80e6c97cb7362
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2023-06-13 16:48:51 -0300

    criando categoria delete parte 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/views/home/home.component';
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'categorias/create',
     component: CategoriaCreateComponent
+  },
+  {
+    path: 'categorias/delete/:id',
+    component: CategoriaDeleteComponent
   }
 ];
 
